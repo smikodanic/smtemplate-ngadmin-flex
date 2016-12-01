5627f92e4e61dafcbcfec6ff3e1bd4b3222fe0b2
smikodanic <smikodanic@gmail.com>
2016-12-01 15:37:59 +0100

    header

--- ng.js
+++ ng.js
@@ -2,8 +2,20 @@
 var clientApp = angular.module('clientApp', []);
 
 
-////// CONTROLLERS
-clientApp.controller('mainCtrl', function ($scope, $window) {
+/* ------ CONTROLLERS ----- */
+clientApp.controller('mainCtrl', function ($scope) {
     'use strict';
     $scope.myVar = 'Something goood!';
+
+    $scope.toggleSidebar = function () {
+        $scope.hidesidebarClass = (!$scope.hidesidebarClass)
+            ? 'hide-sidebar'
+            : '';
+    };
+
+    $scope.toggleDropdownMenu = function () {
+        $scope.inOpenClass = (!$scope.inOpenClass)
+            ? 'in open'
+            : '';
+    };
 });
