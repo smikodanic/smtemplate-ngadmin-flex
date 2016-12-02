57652a4e9f59c0538cc469c8b69171d2b7df0a1c
smikodanic <smikodanic@gmail.com>
2016-12-02 20:55:20 +0100

    sidebar improved

--- ng.js
+++ ng.js
@@ -7,25 +7,28 @@ clientApp.controller('mainCtrl', function ($scope, $timeout) {
     'use strict';
     $scope.myVar = 'Something goood!';
 
+
     $scope.toggleDropdownMenu = function () {
         $scope.inOpenClass = (!$scope.inOpenClass)
             ? 'in open'
             : '';
     };
 
+
     $scope.toggleSidebar = function () {
         if (!$scope.hidesidebarClass) {
             $scope.hidesidebarClass = 'hide-sidebar';
         } else {
             $scope.hidesidebarClass = '';
-            angular.element(document.querySelector('#sidebar *')).css('opacity', 0);
+            angular.element(document.querySelectorAll('#sidebar > .menu-section')).css('opacity', 0);
             $timeout(function () {
-                angular.element(document.querySelector('#sidebar *')).css({opacity: 1, transition: 'all 0.8s ease'});
+                angular.element(document.querySelectorAll('#sidebar > .menu-section')).css({opacity: 1, transition: 'all 0.8s ease'});
             }, 800);
 
         }
 
     };
+
 
     $scope.toggleSidebarChild = function ($event) {
         $event.preventDefault();
@@ -34,9 +37,11 @@ clientApp.controller('mainCtrl', function ($scope, $timeout) {
         if (angular.element($event.currentTarget).parent('li').hasClass('active')) {
             angular.element($event.currentTarget).parent('li').removeClass('active');
         } else {
+            // angular.element(document.querySelector('li.active')).removeClass('active'); //remove all active
             angular.element($event.currentTarget).parent('li').addClass('active');
         }
     };
+
 
     $scope.toggleSidebarGrandChild = function ($event) {
         $event.preventDefault();
